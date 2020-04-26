603c18fd83c906060c886d4e5cd215649ad7920b
SSSemendyaev <14sam88@mail.ru>
2020-04-26 12:43:15 +0300

    order by id, prodDate, speed and rating

--- space/src/main/java/com/space/controller/ShipController.java
+++ space/src/main/java/com/space/controller/ShipController.java
@@ -1,45 +1,47 @@
 package com.space.controller;
 
-import com.space.model.Ship;
-import com.space.service.ShipService;
+import com.space.model.ShipModel;
+import com.space.repository.ShipRepository;
+import org.hibernate.criterion.Order;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.data.domain.Sort;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@RequestMapping(path = "/rest")
 public class ShipController {
-
-    private final ShipService shipService;
-
     @Autowired
-    public ShipController(ShipService shipService) {
-        this.shipService = shipService;
+    ShipRepository shipRepository;
+
+    @GetMapping(path = "/ships")
+    @ResponseBody
+    List<ShipModel> getShips(@RequestParam(required = false) String name,
+                             @RequestParam(required = false) String planet,
+                             @RequestParam(required = false) Integer after,
+                             @RequestParam(required = false) Integer before,
+                             @RequestParam(required = false) Boolean isUsed,
+                             @RequestParam(required = false) Double minSpeed,
+                             @RequestParam(required = false) Double maxSpeed,
+                             @RequestParam(required = false) Integer minCrewSize,
+                             @RequestParam(required = false) Integer maxCrewSize,
+                             @RequestParam(required = false) Double minRating,
+                             @RequestParam(required = false) Double maxRating,
+                             @RequestParam(required = false) Integer pageNumber,
+                             @RequestParam(required = false) Integer pageSize,
+                             @RequestParam(required = false) String order) {
+        if (order != null) {
+            String property = (order.equals("DATE") ? "prodDate" : order.toLowerCase());
+            Sort sort = new Sort(Sort.Direction.ASC, property);
+            sort.getOrderFor(property);
+            return shipRepository.findAll(sort);
+        }
+        return shipRepository.findAll();
     }
 
-    @PostMapping(value = "/ships")
-    public ResponseEntity<?> create(@RequestBody Ship ship) {
-        shipService.create(ship);
-        return new ResponseEntity<>(HttpStatus.CREATED);
-    }
-
-    @GetMapping(value = "/ships")
-    public ResponseEntity<List<Ship>> read() {
-        final List<Ship> ships = shipService.readAll();
-
-        return ships != null && !ships.isEmpty()
-                ? new ResponseEntity<>(ships, HttpStatus.OK)
-                : new ResponseEntity<>(HttpStatus.NOT_FOUND);
-    }
-
-    @GetMapping(value = "/ships/{id}")
-    public ResponseEntity<Ship> read(@PathVariable(name = "id") int id) {
-        final Ship ship = shipService.read(id);
-
-        return ship != null
-                ? new ResponseEntity<>(ship, HttpStatus.OK)
-                : new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    @GetMapping(path = "/ships/count")
+    Long getCount() {
+        return shipRepository.count();
     }
 }
---
+++ space/src/main/java/com/space/model/ShipModel.java
@@ -0,0 +1,40 @@
+package com.space.model;
+
+import org.springframework.context.annotation.Bean;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table(name = "ship")
+public class ShipModel {
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "planet")
+    private String planet;
+
+    @Column(name = "shipType")
+    @Enumerated(EnumType.STRING)
+    private ShipType shipType;
+
+    @Column(name = "prodDate")
+    private Date prodDate;
+
+    @Column(name = "isUsed")
+    private Boolean isUsed;
+
+    @Column(name = "speed")
+    private Double speed;
+
+    @Column(name = "crewSize")
+    private Integer crewSize;
+
+    @Column(name = "rating")
+    private Double rating;
+}
