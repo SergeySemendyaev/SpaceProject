c9f474c64b5a0f9f5a3c926976228f54bea4db28
SSSemendyaev <14sam88@mail.ru>
2020-05-03 23:54:09 +0300

    refactoring
    Signed-off-by: SSSemendyaev <14sam88@mail.ru>

--- space/src/main/java/com/space/service/ShipService.java
+++ space/src/main/java/com/space/service/ShipService.java
@@ -7,7 +7,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
-
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.Predicate;
 import javax.persistence.criteria.Root;
