efbacd50a17642ea183698f53e87cf887bbf7b1e
SSSemendyaev <14sam88@mail.ru>
2020-05-03 23:53:10 +0300

    refactoring
    Signed-off-by: SSSemendyaev <14sam88@mail.ru>

--- space/src/main/java/com/space/service/ShipService.java
+++ space/src/main/java/com/space/service/ShipService.java
@@ -7,7 +7,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
+
+import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
 import java.util.*;
 
 @Service
@@ -30,52 +33,54 @@ public class ShipService {
             Double minRating, Double maxRating
     ) {
 
-        return shipRepository.count((Specification<ShipModel>) (root, query, criteriaBuilder) -> {
-            Predicate predicate = criteriaBuilder.conjunction();
-            if (name != null) {
-                predicate = criteriaBuilder.and(predicate, criteriaBuilder.like(root.get("name"), "%" + name + "%"));
-            }
-            if (planet != null) {
-                predicate = criteriaBuilder.and(predicate, criteriaBuilder.like(root.get("planet"), "%" + planet + "%"));
-            }
-            if (after != null) {
-                Calendar calendar = Calendar.getInstance();
-                calendar.setTimeInMillis(after);
-                Date startDate = calendar.getTime();
-                predicate = criteriaBuilder.and(predicate, criteriaBuilder.greaterThanOrEqualTo(root.get("prodDate"), startDate));
-            }
-            if (before != null) {
-                Calendar calendar = Calendar.getInstance();
-                calendar.setTimeInMillis(before);
-                Date endDate = calendar.getTime();
-                predicate = criteriaBuilder.and(predicate, criteriaBuilder.lessThanOrEqualTo(root.get("prodDate"), endDate));
-            }
-            if (shipType != null) {
-                predicate = criteriaBuilder.and(predicate, criteriaBuilder.equal(root.get("shipType"), shipType));
-            }
-            if (isUsed != null) {
-                predicate = criteriaBuilder.and(predicate, criteriaBuilder.equal(root.get("isUsed"), isUsed));
-            }
-            if (minSpeed != null) {
-                predicate = criteriaBuilder.and(predicate, criteriaBuilder.greaterThanOrEqualTo(root.get("speed"), minSpeed));
-            }
-            if (maxSpeed != null) {
-                predicate = criteriaBuilder.and(predicate, criteriaBuilder.lessThanOrEqualTo(root.get("speed"), maxSpeed));
-            }
-            if (minCrewSize != null) {
-                predicate = criteriaBuilder.and(predicate, criteriaBuilder.greaterThanOrEqualTo(root.get("crewSize"), minCrewSize));
-            }
-            if (maxCrewSize != null) {
-                predicate = criteriaBuilder.and(predicate, criteriaBuilder.lessThanOrEqualTo(root.get("crewSize"), maxCrewSize));
-            }
-            if (minRating != null) {
-                predicate = criteriaBuilder.and(predicate, criteriaBuilder.greaterThanOrEqualTo(root.get("rating"), minRating));
-            }
-            if (maxRating != null) {
-                predicate = criteriaBuilder.and(predicate, criteriaBuilder.lessThanOrEqualTo(root.get("rating"), maxRating));
-            }
-            return predicate;
-        });
+        return shipRepository.count((Specification<ShipModel>) (root, query, criteriaBuilder) -> getPredicate(name, planet, shipType, after, before, isUsed, minSpeed, maxSpeed, minCrewSize, maxCrewSize, minRating, maxRating, root, criteriaBuilder));
+    }
+
+    private Predicate getPredicate(String name, String planet, ShipType shipType, Long after, Long before, Boolean isUsed, Double minSpeed, Double maxSpeed, Integer minCrewSize, Integer maxCrewSize, Double minRating, Double maxRating, Root<ShipModel> root, CriteriaBuilder criteriaBuilder) {
+        Predicate predicate = criteriaBuilder.conjunction();
+        if (name != null) {
+            predicate = criteriaBuilder.and(predicate, criteriaBuilder.like(root.get("name"), "%" + name + "%"));
+        }
+        if (planet != null) {
+            predicate = criteriaBuilder.and(predicate, criteriaBuilder.like(root.get("planet"), "%" + planet + "%"));
+        }
+        if (after != null) {
+            Calendar calendar = Calendar.getInstance();
+            calendar.setTimeInMillis(after);
+            Date startDate = calendar.getTime();
+            predicate = criteriaBuilder.and(predicate, criteriaBuilder.greaterThanOrEqualTo(root.get("prodDate"), startDate));
+        }
+        if (before != null) {
+            Calendar calendar = Calendar.getInstance();
+            calendar.setTimeInMillis(before);
+            Date endDate = calendar.getTime();
+            predicate = criteriaBuilder.and(predicate, criteriaBuilder.lessThanOrEqualTo(root.get("prodDate"), endDate));
+        }
+        if (shipType != null) {
+            predicate = criteriaBuilder.and(predicate, criteriaBuilder.equal(root.get("shipType"), shipType));
+        }
+        if (isUsed != null) {
+            predicate = criteriaBuilder.and(predicate, criteriaBuilder.equal(root.get("isUsed"), isUsed));
+        }
+        if (minSpeed != null) {
+            predicate = criteriaBuilder.and(predicate, criteriaBuilder.greaterThanOrEqualTo(root.get("speed"), minSpeed));
+        }
+        if (maxSpeed != null) {
+            predicate = criteriaBuilder.and(predicate, criteriaBuilder.lessThanOrEqualTo(root.get("speed"), maxSpeed));
+        }
+        if (minCrewSize != null) {
+            predicate = criteriaBuilder.and(predicate, criteriaBuilder.greaterThanOrEqualTo(root.get("crewSize"), minCrewSize));
+        }
+        if (maxCrewSize != null) {
+            predicate = criteriaBuilder.and(predicate, criteriaBuilder.lessThanOrEqualTo(root.get("crewSize"), maxCrewSize));
+        }
+        if (minRating != null) {
+            predicate = criteriaBuilder.and(predicate, criteriaBuilder.greaterThanOrEqualTo(root.get("rating"), minRating));
+        }
+        if (maxRating != null) {
+            predicate = criteriaBuilder.and(predicate, criteriaBuilder.lessThanOrEqualTo(root.get("rating"), maxRating));
+        }
+        return predicate;
     }
 
     public List<ShipModel> getShips(
@@ -92,63 +97,7 @@ public class ShipService {
     ) {
 
         return shipRepository.findAll((Specification<ShipModel>) (root, query, criteriaBuilder) -> {
-            Predicate predicate = criteriaBuilder.conjunction();
-            if (name != null) {
-                predicate = criteriaBuilder.and(predicate, criteriaBuilder.like(root.get("name"), "%" + name + "%"));
-            }
-            if (planet != null) {
-                predicate = criteriaBuilder.and(predicate, criteriaBuilder.like(root.get("planet"), "%" + planet + "%"));
-            }
-            if (after != null) {
-                Calendar calendar = Calendar.getInstance();
-                calendar.setTimeInMillis(after);
-                //calendar.set(Calendar.MONTH, Calendar.JANUARY);
-                //calendar.set(Calendar.DATE, 1);
-                //calendar.set(Calendar.HOUR, 0);
-                //calendar.set(Calendar.MINUTE, 0);
-                //calendar.set(Calendar.SECOND, 0);
-                //calendar.set(Calendar.MILLISECOND, 0);
-                //calendar.set(Calendar.AM_PM, Calendar.AM);
-                Date startDate = calendar.getTime();
-                predicate = criteriaBuilder.and(predicate, criteriaBuilder.greaterThanOrEqualTo(root.get("prodDate"), startDate));
-            }
-            if (before != null) {
-                Calendar calendar = Calendar.getInstance();
-                calendar.setTimeInMillis(before);
-                //calendar.set(Calendar.MONTH, Calendar.DECEMBER);
-                //calendar.set(Calendar.DATE, 31);
-                //calendar.set(Calendar.HOUR, 11);
-                //calendar.set(Calendar.MINUTE, 59);
-                //calendar.set(Calendar.SECOND, 59);
-                //calendar.set(Calendar.MILLISECOND, 999);
-                //calendar.set(Calendar.AM_PM, Calendar.PM);
-                Date endDate = calendar.getTime();
-                predicate = criteriaBuilder.and(predicate, criteriaBuilder.lessThanOrEqualTo(root.get("prodDate"), endDate));
-            }
-            if (shipType != null) {
-                predicate = criteriaBuilder.and(predicate, criteriaBuilder.equal(root.get("shipType"), shipType));
-            }
-            if (isUsed != null) {
-                predicate = criteriaBuilder.and(predicate, criteriaBuilder.equal(root.get("isUsed"), isUsed));
-            }
-            if (minSpeed != null) {
-                predicate = criteriaBuilder.and(predicate, criteriaBuilder.greaterThanOrEqualTo(root.get("speed"), minSpeed));
-            }
-            if (maxSpeed != null) {
-                predicate = criteriaBuilder.and(predicate, criteriaBuilder.lessThanOrEqualTo(root.get("speed"), maxSpeed));
-            }
-            if (minCrewSize != null) {
-                predicate = criteriaBuilder.and(predicate, criteriaBuilder.greaterThanOrEqualTo(root.get("crewSize"), minCrewSize));
-            }
-            if (maxCrewSize != null) {
-                predicate = criteriaBuilder.and(predicate, criteriaBuilder.lessThanOrEqualTo(root.get("crewSize"), maxCrewSize));
-            }
-            if (minRating != null) {
-                predicate = criteriaBuilder.and(predicate, criteriaBuilder.greaterThanOrEqualTo(root.get("rating"), minRating));
-            }
-            if (maxRating != null) {
-                predicate = criteriaBuilder.and(predicate, criteriaBuilder.lessThanOrEqualTo(root.get("rating"), maxRating));
-            }
+            Predicate predicate = getPredicate(name, planet, shipType, after, before, isUsed, minSpeed, maxSpeed, minCrewSize, maxCrewSize, minRating, maxRating, root, criteriaBuilder);
             query.orderBy(criteriaBuilder.asc(root.get(property)));
             return predicate;
         }, pageable).getContent();
